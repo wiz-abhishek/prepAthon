2f7ea81b1880d2a03e67cb5d9f1f2a2be68151ee
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Air-2.local>
2023-08-25 18:07:27 +0530

    Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useDeferredValue } from 'react';
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
+import './App.css';
 
-const App = () => {
+function App() {
+  const [user,setUser]=useState({});
+
+
+  function handleCallBackResponse(response){
+    console.log("Encoded JWT ID token : " + response.credential);
+    var userObject=jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden=true;
+  }
+
+  function handleSignOut(event){
+      setUser({});
+      document.getElementById("signInDiv").hidden=false;
+  }
+  useEffect(()=>{
+    google.accounts.id.initialize({
+      client_id:"34800752652-r2srcuo6lqp6j4csovrdst212agbbqb5.apps.googleusercontent.com",
+      callback : handleCallBackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size :"large"}
+    );
+  }
+  ,[]);
   return (
-    <div>App</div>
+    <div className="App">
+      <div id="signInDiv"></div>
+      {Object.keys(user).length!=0 &&
+        <button onClick={(e)=>handleSignOut(e)}>Sign Out </button>
+      }
+      
+      {
+        user && 
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
+    </div>
   )
 }
 
---
+++ client/src/componets/login.js
@@ -0,0 +1,7 @@
+import {GoogleLogin} from 'react-google-login';
+
+const clientId= "http://34800752652-r2srcuo6lqp6j4csovrdst212agbbqb5.apps.googleusercontent.com";
+
+function login(){
+    
+}
