b8f7dd2fcde5c5c972a9738e5992f6d242588b1d
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-08-25 18:09:46 +0530

    navbar component added

---
+++ client/src/Components/NavbarIcon.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const NavbarIcon = ({ iconSrc }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div 
+      className="flex flex-col items-center space-y-1"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img src={iconSrc} alt="Icon" className="w-8 h-8" />
+      {hover && <div className="w-3 h-3 rounded-full bg-customRed"></div>}
+    </div>
+  );
+};
+
+export default NavbarIcon;
