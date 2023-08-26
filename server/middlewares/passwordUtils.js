0259674af4c7ebf33d4a8002924bf4d8f3524807
shruti sharma <98698727+ShrutiSharma77@users.noreply.github.com>
2023-08-26 12:16:41 +0530

    Create passwordUtils.js

---
+++ server/middlewares/passwordUtils.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+
+function generateHash(password) {
+  var salt = crypto.randomBytes(32).toString("hex");
+  var genHash = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+
+  return {
+    salt: salt,
+    hash: genHash,
+  };
+}
+
+function validatePassword(password, hash, salt) {
+  var hashVerify = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+  return hash === hashVerify;
+}
+
+module.exports.generateHash = generateHash;
+module.exports.validatePassword = validatePassword;
